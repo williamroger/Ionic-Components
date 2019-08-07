ec1d8621035ac1f5141a403300b70b05bf3e1116
William Roger <williamroger.frontend@gmail.com>
2019-08-07 07:52:50 -0300

    ion-skeleton-text

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,6 +1,7 @@
 import { Componente } from 'src/app/interfaces/interfaces';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { delay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -22,6 +23,6 @@ export class DataService {
   }
 
   getHeroes() {
-    return this.http.get('/assets/data/superheroes.json');
+    return this.http.get('/assets/data/superheroes.json').pipe(delay(2000));
   }
 }

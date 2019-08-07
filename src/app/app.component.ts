249752a856456d1ff4804219d115a8949c0eab50
William Roger <williamroger.frontend@gmail.com>
2019-08-07 08:47:50 -0300

    ion-split-pane

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,24 @@
+import { DataService } from 'src/app/services/data.service';
 import { Component } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Observable } from 'rxjs';
+import { Componente } from './interfaces/interfaces';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
+  componentes: Observable<Componente[]>;
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private dataService: DataService
   ) {
     this.initializeApp();
   }
@@ -21,6 +27,8 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+
+      this.componentes = this.dataService.getMenuOptions();
     });
   }
 }

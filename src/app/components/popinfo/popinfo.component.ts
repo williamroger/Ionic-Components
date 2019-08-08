ce1b76c6363df82fa3bf2b2d1a91a3f850cd9da6
William Roger <williamroger.frontend@gmail.com>
2019-08-08 00:13:45 -0300

    ion-popover (continuação)

--- src/app/components/popinfo/popinfo.component.ts
+++ src/app/components/popinfo/popinfo.component.ts
@@ -1,3 +1,4 @@
+import { PopoverController } from '@ionic/angular';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./popinfo.component.scss'],
 })
 export class PopinfoComponent implements OnInit {
+  items = Array(40);
 
-  constructor() { }
+  constructor(private popoverCtrl: PopoverController) { }
 
   ngOnInit() {}
 
+  onClick(valor: number) {
+    console.log('item ', valor);
+
+    this.popoverCtrl.dismiss({
+      item: valor
+    });
+  }
 }
--- src/app/pages/popover/popover.page.ts
+++ src/app/pages/popover/popover.page.ts
@@ -14,11 +14,19 @@ export class PopoverPage implements OnInit {
   ngOnInit() {
   }
 
-  async mostrarPop() {
+  async mostrarPop(evento) {
     const popover = await this.popoverCtrl.create({
       component: PopinfoComponent,
+      event: evento,
+      mode: 'ios',
+      backdropDismiss: false
     });
 
     await popover.present();
+
+    // const { data } = await popover.onDidDismiss();
+    const { data } = await popover.onWillDismiss();
+
+    console.log('Pai ', data);
   }
 }
